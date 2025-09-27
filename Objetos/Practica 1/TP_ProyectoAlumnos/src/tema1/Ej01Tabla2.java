4ab18f95f73ccc023b8b9a0d10403ec100e79ce2
Juan <juan.di176839@alumnos.info.unlp.edu.ar>
2025-09-27 18:00:02 -0300

    Auto-commit 2025-09-27 18:00:02

--- Objetos/Practica 1/TP_ProyectoAlumnos/src/tema1/Ej01Tabla2.java
+++ Objetos/Practica 1/TP_ProyectoAlumnos/src/tema1/Ej01Tabla2.java
@@ -16,7 +16,7 @@ public class Ej01Tabla2 {
         int i;
         for (i=0;i<DF;i++) 
             tabla2[i]=2*i;
-        System.out.println("2x" + "5" + "="+ tabla2[5]);
+        System.out.println("2x" + "5" + "="+ tabla2[2]);
 
     }
     
