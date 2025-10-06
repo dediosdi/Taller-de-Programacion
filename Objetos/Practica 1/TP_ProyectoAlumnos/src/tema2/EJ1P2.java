a98af086474ca0f359bc38438b2b50f08fd59fbc
Juan <juan.di176839@alumnos.info.unlp.edu.ar>
2025-10-06 18:00:01 -0300

    Auto-commit 2025-10-06 18:00:01

---
+++ Objetos/Practica 1/TP_ProyectoAlumnos/src/tema2/EJ1P2.java
@@ -0,0 +1,41 @@
+/*
+ 1- Se dispone de la clase Persona (en la carpeta tema2). Un objeto persona puede crearse
+sin valores iniciales o enviando en el mensaje de creación el nombre, DNI y edad (en ese
+orden). Un objeto persona responde a los siguientes mensajes:
+getNombre() retorna el nombre (String) de la persona
+getDNI() retorna el dni (int) de la persona
+getEdad() retorna la edad (int) de la persona
+setNombre(X) modifica el nombre de la persona al “String” pasado por parámetro (X)
+setDNI(X) modifica el DNI de la persona al “int” pasado por parámetro (X)
+setEdad(X) modifica la edad de la persona al “int” pasado por parámetro (X)
+toString() retorna un String que representa al objeto. Ej: “Mi nombre es Mauro, mi
+DNI es 11203737 y tengo 70 años”
+Realice un programa que cree un objeto persona con datos leídos desde teclado. Luego
+muestre en consola la representación de ese objeto en formato String.
+ */
+package tema2;
+
+import PaqueteLectura.Lector;
+/**
+ *
+ * @author juan
+ */
+public class EJ1P2 {
+    
+    public static void main(String[] args) {
+
+    System.out.println("Escriba el nombre de la persona");
+    String name = Lector.leerString();
+    
+    System.out.println("Escriba la edad de la persona");
+    int age = Lector.leerInt();
+    
+    System.out.println("Escriba el dni  de la persona");
+    int dni = Lector.leerInt();
+    Persona p1 = new Persona (name, dni, age);
+    
+    
+    System.out.println(p1.toString());
+
+}
+}
---
+++ Objetos/Practica 1/TP_ProyectoAlumnos/src/tema2/EJ2P2.java
@@ -0,0 +1,42 @@
+/*
+ 2- Utilizando la clase Persona. Realice un programa que almacene en un vector a lo sumo
+15 personas. La información (nombre, DNI, edad) se debe generar aleatoriamente hasta
+obtener edad 0. Luego de almacenar la información:
+ - Informe la cantidad de personas mayores de 65 años.
+ - Muestre la representación de la persona con menor DNI.
+ */
+package tema2;
+
+import PaqueteLectura.GeneradorAleatorio;
+/**
+ *
+ * @author juan
+ */
+public class EJ2P2 {
+    
+    public static void main(String[] args) {
+    int i; 
+    Persona [] vecP = new Persona [15]; 
+    GeneradorAleatorio.iniciar();                              //inicia el generador aleatorio
+    
+    for (i=0; i<14; i++){
+        System.out.println("Escriba el nombre de la persona");
+        String name = GeneradorAleatorio.generarString(10);
+        System.out.println("Escriba la edad de la persona");
+        int age = GeneradorAleatorio.generarInt(99);
+        System.out.println("Escriba el dni  de la persona");
+        int dni = GeneradorAleatorio.generarInt(99999999);
+        Persona p = new Persona (name, dni, age);
+        vecP [i] = p;
+        }
+        
+        int cant=0;
+        for (i=0; i<14; i++){
+            if (vecP[i].getEdad() > 65){
+            cant = cant+1;
+        }
+        }
+        System.out.println(cant);
+
+}
+}
